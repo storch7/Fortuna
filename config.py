7d38c771ad9ca649432e63adf7fbcd82e554f758
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-02-02 15:07:22 -0300

    Definição do config

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = Path(__file__).resolve().parent
+load_dotenv(BASE_DIR / ".env")
+
+TOKEN_TELEGRAM = os.getenv("TOKEN_TELEGRAM")
+
+if not TOKEN_TELEGRAM:
+    raise RuntimeError("TOKEN_TELEGRAM não definido no .env")
