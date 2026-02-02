c96862596089aba300e7682682a3d6b712cb498b
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-02-02 15:10:31 -0300

    Confeccção da controller e definição do método main para execução

---
+++ controller/telegram_controller.py
@@ -0,0 +1,18 @@
+from model.api.telegramAPI import TelegramAPI
+
+telegram_api = TelegramAPI()
+bot = telegram_api.bot
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    telegram_api.reply(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    telegram_api.reply(message, message.text)
+
+
+def run():
+    telegram_api.start_polling()
---
+++ main.py
@@ -0,0 +1,4 @@
+from controller.telegram_controller import run
+
+if __name__ == "__main__":
+    run()
