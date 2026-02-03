ae003c30f23e9135dc3215bf27fe4ab138fe4316
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-02-03 09:27:13 -0300

    Adicionando o token notion à aplicação e ao .env.example

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
-from pathlib import Path
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-#load_dotenv(BASE_DIR / ".env.exemple")
 load_dotenv(BASE_DIR / ".env")
 
 TOKEN_TELEGRAM = os.getenv("TOKEN_TELEGRAM")
+TOKEN_NOTION = os.getenv("TOKEN_NOTION")
 
 if not TOKEN_TELEGRAM:
     raise RuntimeError("TOKEN_TELEGRAM não definido no .env")
+
+if not TOKEN_NOTION:
+    raise RuntimeError("TOKEN_NOTION não definido no .env")
